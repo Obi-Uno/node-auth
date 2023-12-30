2a921746434e82941baaf1e43c25b655b0a50be8
Obi-Uno <sarthakrautelanew@gmail.com>
2023-12-31 00:17:46 +0530

    auth routes and controllers
    created route paths and cleaned the code by creating controllers

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes= require('./routes/authRoutes');
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb://0.0.0.0:27017/Node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) =>{ 
+   console.log("db connected");
+    app.listen(3000);
+    
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
